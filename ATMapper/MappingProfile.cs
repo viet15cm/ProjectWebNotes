185e0f6d0d4f15029944bbe071d54f52da42bd5d
viet15cm <viet99cm@gmail.comm>
2023-04-11 16:44:26 +0700

    EditAutoMap

--- ATMapper/MappingProfile.cs
+++ ATMapper/MappingProfile.cs
@@ -8,13 +8,19 @@ namespace ATMapper
     {
         public MappingProfile()
         {
-            CreateMap<Category, CategoryForWithDetailDto>();
-            CreateMap<CategoryForWithDetailDto, Category>();
+            CreateMap<Category, CategoryForWithDetailDto>()
+                .ForMember(x => x.PostCategories, k => k.MapFrom(y => y.PostCategories));
+
+            CreateMap<PostCategory, PostCategoryForWithDetailDto>()
+                .ForMember(x => x.Category, k => k.MapFrom(y => y.Category))
+                .ForMember(x => x.Post, k => k.MapFrom(y => y.Post));
+
+            CreateMap<Post, PostForWithDetailDto>()
+                .ForMember(x => x.PostCategories, k => k.MapFrom(y => y.PostCategories));
+
             CreateMap<Category, CategoryDto>();
             CreateMap<Category, CategoryForUpdateDto>();
-
             CreateMap<Category, CategoryForCreationDto>();
-
             CreateMap<Post, PostDto>();
 
             // All other mappings goes here
